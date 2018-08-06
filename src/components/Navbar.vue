5df159b3073cfd2db6be5264294b245405b43cba
vijairam <vijairam20@gmail.com>
2018-08-07 02:04:37 +0300

    Navbar init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="navbar">
+    <div class="nav-logo">
+      <router-link to = "/"><span>MUNI</span></router-link>
+    </div>
+    <div class="nav-items">
+        <router-link to = "/customers">Customers</router-link>
+        <router-link to = "/resources">Resources</router-link>
+        <router-link to = "/about">About Us</router-link>
+        <router-link to = "/contactus">Contact Us</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name : "Navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Russo+One');
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+.navbar{
+  display: flex;
+  //background-color: #00e0ff;
+  margin: 0px;
+  justify-content: space-between;
+}
+.nav-logo{
+  margin-top: 0.6em;
+  span {
+      font-family: 'Russo One' , sans-serif ;
+      font-size: 3em;
+      padding: 1em;
+    }
+  a{
+    text-decoration: none ;
+    color: black;
+  }
+}
+.nav-items{
+
+  display: flex;
+  flex-direction: flex-end;
+  font-family: 'Montserrat' , sans-serif ;
+  a{
+    text-decoration: none ;
+    color: black;
+    padding: 1em;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+}
+</style>
+
