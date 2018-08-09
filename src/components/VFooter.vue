c140aa3bda0d646c3510e27ad360884462af5d88
vijairam <vijairam20@gmail.com>
2018-08-10 01:04:54 +0300

    vfooter init

---
+++ src/components/VFooter.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="vfooter">
+     <div class="cta">
+        <div class="logo">
+          <span>MUNI</span>
+        </div>
+        <div class="email">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet exercitationem laborum ab quam voluptate </p>
+        </div>
+      </div>
+      <div class="bottom">
+    <div class="nav-items">
+      
+        <router-link to = "/">Home</router-link>
+        <router-link to = "/customers">Customers</router-link>
+        <router-link to = "/resources">Resources</router-link>
+        <router-link to = "/about">About Us</router-link>
+        <router-link to = "/contactus">Contact Us</router-link>
+    </div>
+  </div>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+@import url('https://fonts.googleapis.com/css?family=Russo+One');
+.cta{
+  margin-top: 30px;
+    display: flex;
+    background-color: #3f51b5;
+    padding: 4em;
+    text-align: center;
+      font-family: 'Montserrat', sans-serif;
+       color: white;
+
+}
+
+.logo{
+  span {
+      font-family: 'Russo One' , sans-serif ;
+      font-size: 4em;
+      padding: 3em;
+
+    }
+}
+
+.email{
+text-align: left ;
+font-size: 1.5em;
+padding-left: 5em;
+}
+
+.bottom{
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: flex-end;
+  font-family: 'Montserrat', sans-serif;
+
+  h1{
+    margin-right: 10px;
+    color: white;
+    font-size: 1em;
+    padding: 1em;
+  }
+}
+
+.nav-logo{
+  margin-top: 0.6em;
+  span {
+      font-family: 'Russo One' , sans-serif ;
+      font-size: 3em;
+      padding: 1em;
+    }
+  a{
+    text-decoration: none ;
+    color: white;
+  }
+}
+.nav-items{
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  font-family: 'Montserrat' , sans-serif ;
+
+  a{
+    text-decoration: none ;
+    padding: 1em;
+    font-weight: bold;
+    font-size: 1.5em;
+    color: white;
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="home">
     <hero/>
-    <CBody/>
+     <CBody/> 
     <people/>
-    <footer>
-      <div class="cta">
-        <div class="logo">
-          <span>MUNI</span>
-        </div>
-        <div class="email">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet exercitationem laborum ab quam voluptate </p>
-        </div>
-      </div>
-      <div class="bottom">
-        <h1>MUNI</h1>
-      </div>
-    </footer>
+    <VFooter/> 
   </div>
 </template>
 
@@ -24,50 +12,19 @@ import People from "@/components/People.vue"
 import Hero from "@/components/Hero.vue"
 import Navbar from "@/components/Navbar.vue"
 import CBody from "@/components/Cbody.vue"
+import VFooter from "@/components/VFooter.vue"
 
 export default {
   name: "home",
   components: {
-    People , Hero , Navbar , CBody
+    People , Hero , Navbar , CBody , VFooter
   }
 };
 </script>
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Montserrat');
-@import url('https://fonts.googleapis.com/css?family=Open+Sans');
-@import url('https://fonts.googleapis.com/css?family=Russo+One');
 
-footer{
-  margin-top: 20px;
-  //height: 200px;
-  font-family: 'Montserrat', sans-serif;
-  color: white;
 
-}
-.cta{
-  display: flex;
-  background-color:#3F51B5;
-  padding: 4em;
-  text-align: center;
-}
-.logo{
-  span {
-      font-family: 'Russo One' , sans-serif ;
-      font-size: 4em;
-      padding: 3em;
 
-    }
-}
-.email{
-text-align: left ;
-font-size: 1.5em;
-padding-left: 5em;
-}
-
-.bottom{
-  display: flex;
-  color: black;
-}
 
 
 </style>
